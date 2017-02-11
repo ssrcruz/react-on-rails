37aace99e771b8bd337427603caf47b7180b6c7b
Ruben <ssrcruz@gmail.com>
2017-02-11 00:40:24 -0500

    React on Rails CRUD Example

---
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -0,0 +1,23 @@
+var NewItem = React.createClass({
+  handleClick() {
+    var name = this.refs.name.value;
+    var description = this.refs.description.value;
+    $.ajax({
+      url: '/api/v1/items',
+      type: 'POST',
+      data: { item: { name: name, description: description } },
+      success: (item) => {
+        this.props.handleSubmit(item);
+      }
+    });
+  },
+  render() {
+    return (
+      <div>
+        <input ref='name' placeholder='Enter item name' />
+        <input ref='description' placeholder='Enter a description' />
+        <button onClick={this.handleClick}>Submit</button>
+      </div>
+    )
+  }
+});
